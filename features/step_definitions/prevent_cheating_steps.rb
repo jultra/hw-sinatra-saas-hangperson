1d4060c74b7ce214bb4b082e44b7add14fd7b6d0
Juan Pinzon <juancpinzone@berkeley.edu>
2015-07-23 05:34:18 -0700

    Rspec

--- features/step_definitions/prevent_cheating_steps.rb
+++ features/step_definitions/prevent_cheating_steps.rb
@@ -1,13 +1,12 @@
-Given(/^I start a new game with word "([^"]*)"$/) do |arg1|
-
-  pending # Write code here that turns the phrase above into concrete actions
-
+When /^I start a new game with word "(.*)"$/ do |word|
+  stub_request(:post, "http://watchout4snakes.com/wo4snakes/Random/RandomWord").
+    to_return(:status => 200, :headers => {}, :body => word)
+  visit '/new'
+  click_button "New Game"
 end
 
-When(/^I try to go to the URL "([^"]*)"$/) do |arg1|
-
-  pending # Write code here that turns the phrase above into concrete actions
-
+When /^I try to go to the URL "(.*)"$/ do |url|
+  visit url
 end
 
 Then /^(?:|I )should be on (.+)$/ do |page_name|
