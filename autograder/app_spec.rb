80d9b0de32720dfeeba446e86bc8e83a232c46c7
JUCHAN KIM <yj1877@berkeley.edu>
2015-08-11 13:15:34 -0700

    add in the points

--- autograder/app_spec.rb
+++ autograder/app_spec.rb
@@ -70,12 +70,12 @@ describe "Hangperson" do
   end
 
   describe "game over" do
-    it "because the word is guessed [5 points]" do
+    it "because the word is guessed [5 points]" , points: 5 do
       new_game "foobar"
       guesses "fobar"
       expect(current_route).to eq("/win")
     end
-    it "because the guess limit is reached [5 points]" do
+    it "because the guess limit is reached [5 points]" , points: 5 do
       new_game "zebra"
       guesses "isuckhar"
       expect(current_route).to eq("/show")
@@ -85,28 +85,28 @@ describe "Hangperson" do
   end
 
   describe "guessing" do
-    it "should reveal the letter if it is correct [5 points]" do
+    it "should reveal the letter if it is correct [5 points]" , points: 5 do
       new_game "garply"
       guess "g"
       expect(word_board).to eq("g-----")
     end
-    it "should reveal the letter if it appears many times [5 points]" do
+    it "should reveal the letter if it appears many times [5 points]" , points: 5 do
       new_game "animal"
       guess "a"
       expect(word_board).to eq("a---a-") 
     end
-    it "should add to wrong guesses when incorrect [5 points]" do
+    it "should add to wrong guesses when incorrect [5 points]" , points: 5 do
       new_game "xylophone"
       guess "a"
       expect(wrong_guesses).to eq("a")
     end
-    it "should continuously track right and wrong guesses [5 points]" do
+    it "should continuously track right and wrong guesses [5 points]" , points: 5 do
       new_game "foobar"
       guesses "azxo"
       expect(word_board).to eq("-oo-a-")
       expect(wrong_guesses).to eq("zx")
     end
-    it "should treat guesses as case insensitive [5 points]" do
+    it "should treat guesses as case insensitive [5 points]" , points: 5 do
       new_game "fiddlesticks"
       guess "t"
       word_board1 = word_board
@@ -122,27 +122,27 @@ describe "Hangperson" do
   end
 
   describe "guessing repeats" do
-    it "should not count as wrong when correct [5 points]" do
+    it "should not count as wrong when correct [5 points]" , points: 5 do
       new_game "bumblebee"
       guess "b"
       guess "b"
       guess "b"
       expect(word_board).to eq("b--b--b--")
     end
-    it "should not double count a repeated wrong guess [5 points]" do
+    it "should not uble count a repeated wrong guess [5 points]" , points: 5 do
       new_game "giraffe"
       guess "z"
       guess "z"
       expect(wrong_guesses).to eq("z")
     end
-    it "should not count a repeated wrong guess towards losing [5 points]" do
+    it "should not count a repeated wrong guess towards losing [5 points]" , points: 5 do
       new_game "snake"
       30.times do
         guess "z"
       end
       expect(current_route).to eq("/show")
     end
-    it "should display an error message [5 points]" do
+    it "should display an error message [5 points]" , points: 5 do
       new_game "figs"
       guess "i"
       guess "i"
@@ -151,12 +151,12 @@ describe "Hangperson" do
   end
 
   describe "invalid guess" do
-    it "should label a non-letter as invalid [5 points]" do
+    it "should label a non-letter as invalid [5 points]" , points: 5 do
       new_game "helterskelter"
       guess "^"
       expect(flash_message).to eq("Invalid guess.")
     end
-    it "should label a blank form as invalid [5 points]" do
+    it "should label a blank form as invalid [5 points]" , points: 5 do
       new_game "pumpkin"
       guess ""
       expect(flash_message).to eq("Invalid guess.")
@@ -164,19 +164,19 @@ describe "Hangperson" do
   end
 
   describe "starting a new game" do
-    it "should take me to the show page [5 points]" do
+    it "should take me to the show page [5 points]" , points: 5 do
       click_new_game
       expect(current_route).to eq("/show")
     end
   end
 
   describe "cheating" do
-    it "should not be able to fake a win [5 points]" do
+    it "should not be able to fake a win [5 points]" , points: 5 do
       new_game "math"
       goto "/win"
       expect(current_route).to eq("/show")
     end
-    it "should not be able to fake a loss [5 points]" do
+    it "should not be able to fake a loss [5 points]" , points: 5 do
       new_game "howdy"
       goto "/lose"
       expect(current_route).to eq("/show")
